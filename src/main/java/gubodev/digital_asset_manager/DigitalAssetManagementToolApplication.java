81d5c531ee836138bc653044debc6a2c9d09e5eb
Dev-Cris <83130582+Dev-Cris@users.noreply.github.com>
2024-02-03 19:41:53 +0100

    initializing project

---
+++ src/main/java/gubodev/digital_asset_manager/DigitalAssetManagementToolApplication.java
@@ -0,0 +1,14 @@
+package gubodev.digital_asset_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalAssetManagementToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalAssetManagementToolApplication.class, args);
+        System.out.println("Hi, Mom! :D");
+    }
+
+}
---
+++ src/test/java/gubodev/digital_asset_manager/DigitalAssetManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package gubodev.digital_asset_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalAssetManagementToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
